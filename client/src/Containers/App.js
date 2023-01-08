a4521efcf5cdd6e23d0a369b131560d83401470d
fabiangif <109872777+fabiangif@users.noreply.github.com>
2023-01-08 16:17:49 -0500

    Modularizando

--- client/src/Containers/App.js
+++ client/src/Containers/App.js
@@ -18,7 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<Inprocess />} />
+          <Route path="/about" element={<About />} />
           <Route path="/details/:id" element={<MatchDetails />} />
           <Route path="/promotions" element={<Inprocess />} />
           <Route path="/tutorial" element={<Inprocess />} />
